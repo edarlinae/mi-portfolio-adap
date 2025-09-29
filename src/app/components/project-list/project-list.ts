2bb8d68745771fff0b904157706897438ab034d4
edarlinae <edarlinae@gmail.com>
2025-09-29 16:58:39 +0200

    edicion iconos

--- src/app/components/project-list/project-list.ts
+++ src/app/components/project-list/project-list.ts
@@ -6,10 +6,10 @@ import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faAngular, faBootstrap, faCss3Alt, faFigma, faGithub, faHtml5, faJs, faSass } from '@fortawesome/free-brands-svg-icons';
 import { faCode, faEye } from '@fortawesome/free-solid-svg-icons';
 
-// Estructura para las tecnologías con iconos de FontAwesome
 export interface Technology {
   name: string;
   icon: IconDefinition;
+  color: string; // <-- AÑADIR PROPIEDAD DE COLOR
 }
 
 export interface Project {
@@ -23,7 +23,7 @@ export interface Project {
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule], // <-- Añadir FontAwesomeModule
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './project-list.html',
   styleUrl: './project-list.css'
 })
@@ -31,7 +31,6 @@ export class ProjectListComponent {
   private translationService = inject(TranslationService);
   public t = this.translationService.t;
 
-  // Iconos para la plantilla
   faGithub = faGithub;
   faEye = faEye;
 
@@ -40,10 +39,10 @@ export class ProjectListComponent {
       imageUrl: 'assets/clicktocall-preview.png',
       translationKey: 'project1',
       technologies: [
-        { name: 'JavaScript', icon: faJs },
-        { name: 'HTML', icon: faHtml5 },
-        { name: 'CSS', icon: faCss3Alt },
-        { name: 'Chrome API', icon: faCode } // Usando un icono genérico
+        { name: 'JavaScript', icon: faJs, color: '#f7df1e' },
+        { name: 'HTML', icon: faHtml5, color: '#e34f26' },
+        { name: 'CSS', icon: faCss3Alt, color: '#1572b6' },
+        { name: 'Chrome API', icon: faCode, color: '#888' }
       ],
       liveDemoUrl: 'https://chromewebstore.google.com/detail/clicktocall-premium-number/hcdjknjpbnhdmpdkeofgmafdcepdbjfg?hl=es',
       githubUrl: 'https://github.com/edarlinae/ClickToCall'
@@ -52,9 +51,9 @@ export class ProjectListComponent {
       imageUrl: 'assets/weather-app-preview.jpg',
       translationKey: 'project2',
       technologies: [
-        { name: 'Angular', icon: faAngular },
-        { name: 'TypeScript', icon: faCode }, // faCode como sustituto de TS
-        { name: 'Bootstrap', icon: faBootstrap }
+        { name: 'Angular', icon: faAngular, color: '#dd0031' },
+        { name: 'TypeScript', icon: faCode, color: '#3178c6' },
+        { name: 'Bootstrap', icon: faBootstrap, color: '#7952b3' }
       ],
       liveDemoUrl: 'https://weather-app-edarlinae.vercel.app/',
       githubUrl: 'https://github.com/edarlinae/Weather-App'
@@ -63,10 +62,10 @@ export class ProjectListComponent {
       imageUrl: 'assets/apego-consciente.png',
       translationKey: 'project3',
       technologies: [
-        { name: 'Angular', icon: faAngular },
-        { name: 'TypeScript', icon: faCode }, // faCode como sustituto de TS
-        { name: 'SCSS', icon: faSass },
-        { name: 'Figma', icon: faFigma }
+        { name: 'Angular', icon: faAngular, color: '#dd0031' },
+        { name: 'TypeScript', icon: faCode, color: '#3178c6' },
+        { name: 'SCSS', icon: faSass, color: '#cc6699' },
+        { name: 'Figma', icon: faFigma, color: '#f24e1e' }
       ],
       liveDemoUrl: '#',
       githubUrl: 'https://github.com/edarlinae/apego-consciente'
@@ -75,9 +74,9 @@ export class ProjectListComponent {
       imageUrl: 'assets/kairosgab.png',
       translationKey: 'project4',
       technologies: [
-        { name: 'Angular', icon: faAngular },
-        { name: 'TypeScript', icon: faCode }, // faCode como sustituto de TS
-        { name: 'SCSS', icon: faSass }
+        { name: 'Angular', icon: faAngular, color: '#dd0031' },
+        { name: 'TypeScript', icon: faCode, color: '#3178c6' },
+        { name: 'SCSS', icon: faSass, color: '#cc6699' }
       ],
       liveDemoUrl: 'https://www.gabinetekairos.es/',
       githubUrl: 'https://github.com/edarlinae/kairos-web'
