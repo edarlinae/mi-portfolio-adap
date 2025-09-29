d63fd613c459493a55bbf4ce735b93c222f0a2b5
edarlinae <edarlinae@gmail.com>
2025-09-29 16:44:39 +0200

    ajuste botones e iconos 1

--- src/app/components/project-list/project-list.ts
+++ src/app/components/project-list/project-list.ts
@@ -1,11 +1,21 @@
 import { Component, signal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslationService, ProjectTranslations } from '../../services/translation';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faAngular, faBootstrap, faCss3Alt, faFigma, faGithub, faHtml5, faJs, faSass } from '@fortawesome/free-brands-svg-icons';
+import { faCode, faEye } from '@fortawesome/free-solid-svg-icons';
+
+// Estructura para las tecnologías con iconos de FontAwesome
+export interface Technology {
+  name: string;
+  icon: IconDefinition;
+}
 
 export interface Project {
   imageUrl: string;
   translationKey: 'project1' | 'project2' | 'project3' | 'project4';
-  technologies: string[];
+  technologies: Technology[];
   liveDemoUrl: string;
   githubUrl: string;
 }
@@ -13,7 +23,7 @@ export interface Project {
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule], // <-- Añadir FontAwesomeModule
   templateUrl: './project-list.html',
   styleUrl: './project-list.css'
 })
@@ -21,32 +31,54 @@ export class ProjectListComponent {
   private translationService = inject(TranslationService);
   public t = this.translationService.t;
 
+  // Iconos para la plantilla
+  faGithub = faGithub;
+  faEye = faEye;
+
   protected readonly projects = signal<Project[]>([
     {
       imageUrl: 'assets/clicktocall-preview.png',
       translationKey: 'project1',
-      technologies: ['JavaScript', 'HTML', 'CSS', 'API REST'],
+      technologies: [
+        { name: 'JavaScript', icon: faJs },
+        { name: 'HTML', icon: faHtml5 },
+        { name: 'CSS', icon: faCss3Alt },
+        { name: 'Chrome API', icon: faCode } // Usando un icono genérico
+      ],
       liveDemoUrl: 'https://chromewebstore.google.com/detail/clicktocall-premium-number/hcdjknjpbnhdmpdkeofgmafdcepdbjfg?hl=es',
       githubUrl: 'https://github.com/edarlinae/ClickToCall'
     },
     {
       imageUrl: 'assets/weather-app-preview.jpg',
       translationKey: 'project2',
-      technologies: ['Angular', 'TypeScript', 'API REST', 'Bootstrap'],
+      technologies: [
+        { name: 'Angular', icon: faAngular },
+        { name: 'TypeScript', icon: faCode }, // faCode como sustituto de TS
+        { name: 'Bootstrap', icon: faBootstrap }
+      ],
       liveDemoUrl: 'https://weather-app-edarlinae.vercel.app/',
       githubUrl: 'https://github.com/edarlinae/Weather-App'
     },
     {
       imageUrl: 'assets/apego-consciente.png',
       translationKey: 'project3',
-      technologies: ['Angular', 'TypeScript', 'SCSS', 'Figma'],
+      technologies: [
+        { name: 'Angular', icon: faAngular },
+        { name: 'TypeScript', icon: faCode }, // faCode como sustituto de TS
+        { name: 'SCSS', icon: faSass },
+        { name: 'Figma', icon: faFigma }
+      ],
       liveDemoUrl: '#',
       githubUrl: 'https://github.com/edarlinae/apego-consciente'
     },
     {
-      imageUrl: 'assets/kairosgab.png', // <-- Nombre de imagen verificado
+      imageUrl: 'assets/kairosgab.png',
       translationKey: 'project4',
-      technologies: ['Angular', 'TypeScript', 'SCSS'],
+      technologies: [
+        { name: 'Angular', icon: faAngular },
+        { name: 'TypeScript', icon: faCode }, // faCode como sustituto de TS
+        { name: 'SCSS', icon: faSass }
+      ],
       liveDemoUrl: 'https://www.gabinetekairos.es/',
       githubUrl: 'https://github.com/edarlinae/kairos-web'
     }
